54b646c90f64dfb0aa2062fb332f7c3c0a8b2db3
undeo <dem230091@spengergasse.at>
2023-12-14 08:53:36 +0100

    Funktionen im index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 name = "Dejan"
 fullname = namre + "Demirchev"
+
+function Addieren() {
+    var a = 32;
+    var b = 777;
+    var ergebniss = a+b;
+    return ergebnis
+}
+
+var summe = Addieren();
+console.log("die summe ist " + summe)
+
