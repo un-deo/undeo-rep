e7f8e1eca858bc570b8e08a9db707b454a8f058e
undeo <dem230091@spengergasse.at>
2024-02-22 13:50:33 +0100

    update

--- 2024-02-15.webseite-zu-PLf2/plf3-1.js
+++ 2024-02-15.webseite-zu-PLf2/plf3-1.js
@@ -1,56 +1,3 @@
-//class Person {
-//  constructor(name, gender, groesseM, gewichtKG) {
-//    if (gender !== "m" && gender !== "f") {
-//      throw new Error("Wrong gender");
-//    }
-//    this.name = name;
-//    this.gender = gender;
-//    this.groesseM = groesseM;
-//    this.gewichtKG = gewichtKG;
-//  }
-//
-//
-//  groesseCM() {
-//    return this.groesseM * 100;
-//  }
-//
-//  vorName() {
-//    const names = this.name.split(" ");
-//    return names[0];
-//  }
-//
-//  nachName() {
-//    const names = this.name.split(" ");
-//    return names.slice(1).join(" ");
-//  }
-//
-//  toString() {
-//    return `${this.name} (${this.groesseCM()}cm, ${this.gewichtKG}kg)`;
-//  }
-//
-//  getBmi() {
-//    const bmi = this.gewichtKG / this.groesseM ** 2;
-//    return bmi;
-//  }
-//
-//  getGewichtType() {
-//    const bmi2 = this.getBmi();
-//    if (this.gender === "f" && bmi2 >= 19 && bmi2 <= 24) {
-//      return "Normalgewicht";
-//    } else if (this.gender === "m" && bmi2 >= 20 && bmi2 <= 25) {
-//      return "Normalgewicht";
-//    } else if (bmi2 < 19) {
-//      return "Untergewicht";
-//    } else {
-//      return "Übergewicht";
-//    }
-//  }
-//}
-//
-//
-//
-
-
 class Person {
   constructor(name, gender, groesseM, gewichtKG) {
     if (gender !== "male" && gender !== "female") {
@@ -89,13 +36,13 @@ class Person {
   getGewichtType() {
     const bmi2 = this.getBmi();
     if (this.gender === "female" && bmi2 >= 19 && bmi2 <= 24) {
-      return "Normalgewicht";
+      return "Normalgewichtig";
     } else if (this.gender === "male" && bmi2 >= 20 && bmi2 <= 25) {
-      return "Normalgewicht";
+      return "Normalgewichtig";
     } else if (bmi2 < 19) {
-      return "Untergewicht";
+      return "Untergewichtig";
     } else {
-      return "Übergewicht";
+      return "Übergewichtig";
     }
   }
 }
