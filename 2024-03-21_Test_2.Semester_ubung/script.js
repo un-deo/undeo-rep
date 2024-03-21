2b8e820e47b412ce4b47d024153f670c98dace55
undeo <dem230091@spengergasse.at>
2024-03-21 12:25:30 +0100

    Test schleifen und funktionen

---
+++ 2024-03-21_Test_2.Semester_ubung/script.js
@@ -0,0 +1,20 @@
+function Tester(statement1, statement2) {
+    for (let i = 0; i < 15; i++) {
+        console.log("This is for learning purposes only")
+        if (statement1 == 1 && statement2 !== NaN) {
+            console.log("code is Working")
+        } else if (statement1 !== 1) {
+            console.log("Fail")
+        }
+}
+}
+
+ 
+Tester(1, 2);
+
+
+Tester(2, 2)
+
+console.log("end")
+
+
