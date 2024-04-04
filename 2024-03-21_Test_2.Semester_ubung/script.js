3ae77e1c010d48cfa8db81fbb9c685b539b73057
undeo <dem230091@spengergasse.at>
2024-04-04 13:43:09 +0200

    fib class

--- 2024-03-21_Test_2.Semester_ubung/script.js
+++ 2024-03-21_Test_2.Semester_ubung/script.js
@@ -1,15 +1,34 @@
-function fib(n) { 
-    const arr = [0, 1];
-    for (let i = 0; i < n; i++) { 
-        arr.push(arr.at(-1) + arr.at(-2))
+//function fib(n) {
+//    const arr = [0, 1];
+//    for (let i = 0; i < n; i++) {
+//        arr.push(arr.at(-1) + arr.at(-2))
+//    }
+//    console.log(arr[n]);
+//}
+//
+//fib(7)
+//fib(8)
+//fib(21)
+//fib(3)
+//fib(40)
+//
+//console.log("Breakpoint")
+
+class Fib {
+  constructor() {
+    this.fibs = [0, 1];
+  }
+  fib(n) {
+    const wieOft = n - this.fibs.length + 1;
+    for (let i = 0; i < wieOft; i++) {
+      this.fibs.push(this.fibs.at(-1) + this.fibs.at(-2));
     }
-    console.log(arr[n]);
+    return this.fibs.at(n);
+  }
 }
 
-fib(7)
-fib(8)
-fib(21)
-fib(3)
-fib(40)
-
-console.log("Breakpoint")
+fibInstance = new Fib();
+console.log(fibInstance.fib(5));
+console.log(fibInstance.fib(12));
+console.log(fibInstance.fib(8));
+console.log(fibInstance.fib(3));
