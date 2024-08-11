7ba2b53d31de6fe38054f3a8a727a3c412649af8
undeo <dem230091@spengergasse.at>
2024-08-11 14:01:55 +0200

    Tidy up repo

--- SJ-2023-24-1AHWII/2024-06-17 game/game.js
+++ SJ-2023-24-1AHWII/2024-06-17 game/game.js
@@ -4,9 +4,13 @@ let option2 = document.getElementById("option2");
 let option3 = document.getElementById("option3");
 let option4 = document.getElementById("option4");
 let items = document.getElementById("items");
+let keypad = document.getElementById("solveX");
 let haveascrewdriver = false;
 let openedVent = false;
 let haveacrowbar = false;
+let finish = document.getElementById("finish");
+let failedAtempts = 0;
+let keypadinput = document.getElementById("keypadinput");
 
 function start() {
   option1.innerText = "Try to kick in the door.";
@@ -16,6 +20,12 @@ function start() {
   haveascrewdriver = false;
   openedVent = false;
   haveacrowbar = false;
+  items.innerText = "";
+  option3.hidden = false; //show the option again
+  keypad.hidden = true;
+  finish.hidden = true;
+  failedAtempts = 0;
+  keypadinput.value = "";
 }
 
 start();
@@ -23,6 +33,16 @@ start();
 option1.addEventListener("click", function () {
   if (option1.innerText == "Try to kick in the door.") {
     kickDoor();
+  } else if (
+    option1.innerText == "Try to unhinge the door" &&
+    haveacrowbar === false
+  ) {
+    alert("The doors hinges are sturdy. Find another way to open the door.");
+  } else if (
+    option1.innerText == "Try to unhinge the door" &&
+    haveacrowbar === true
+  ) {
+    alert("Even with a crowbar you can't unhinge the door.");
   }
 });
 
@@ -31,13 +51,16 @@ option2.addEventListener("click", function () {
     openChest();
   } else if (option2.innerText == "check behind the painting") {
     alert(
-      "You found a note behind the painting. on it someone has written: lift up the box. there is a hidden compartment beneath it."
+      "You found a note and an input panel behind the painting. on it someone has written: lift up the box. there is a hidden compartment beneath it."
     );
     option2.innerText = "Lift up box";
+    keypad.hidden = false;
   } else if (option2.innerText == "Lift up box" && haveacrowbar === false) {
     alert("the box is nailed to the floor. You need a tool to remove it.");
   } else if (option2.innerText == "Lift up box" && haveacrowbar === true) {
     avterBoxlift();
+  } else if (option2.innerText == "Solve puzzle") {
+    puzzle();
   }
 });
 
@@ -46,6 +69,29 @@ option3.addEventListener("click", function () {
     openVent();
   } else if (option3.innerText == "Climb into the Ventilation shaft") {
     climbInToVent();
+  } else if (
+    option3.innerText == "input answer into the panel the press this button"
+  ) {
+    if (keypadinput.value == "880" || keypadinput.value == 880) {
+      finish.hidden = false;
+    } else if (
+      option3.innerText ==
+        "input answer into the panel the press this button" &&
+      keypadinput.value != 880 &&
+      failedAtempts !== 1
+    ) {
+      alert("You have inputted the wrong answer. you have 1 more try.");
+      failedAtempts++;
+      keypadinput.value = "";
+    } else if (
+      option3.innerText ==
+        "input answer into the panel the press this button" &&
+      keypadinput.value != 880 &&
+      failedAtempts === 1
+    ) {
+      alert("You have inputted the wrong answer. you have failed.");
+      start();
+    }
   }
 });
 
@@ -82,12 +128,9 @@ function climbInToVent() {
     alert("You found a crowbar in the ventilation shaft.");
     haveacrowbar = true;
     items.innerText += " Crowbar";
+    option3.hidden = true; //hide the option temporarily
   } else if (openedVent === false) {
     alert("You need to open the ventilation shaft first.");
-  } else if (haveacrowbar === true) {
-    alert(
-      "It seems that you don't fit in the ventilation shaft. You need to find another way to escape."
-    );
   }
 }
 
@@ -96,4 +139,10 @@ function avterBoxlift() {
     "you have found a hidden compartment beneath the box. You found a puzzle inside it."
   );
   option2.innerText = "Solve puzzle";
+}
+
+function puzzle() {
+  option2.innerText = `Solve for X: "8x - 246 = 6794"`; //answer is 1
+  option3.innerText = "input answer into the panel the press this button";
+  option3.hidden = false;
 }
