a3fd4fe747156750f2dd15b3491ea98220554a64
undeo <dem230091@spengergasse.at>
2025-04-16 12:18:44 +0300

    fancy-blumenshop-Json-file-download

--- 2024-09-10-2AHWII/2024-11-26-Blumengeschaft-bootstrap/script.js
+++ 2024-09-10-2AHWII/2024-11-26-Blumengeschaft-bootstrap/script.js
@@ -1,6 +1,7 @@
 const Obj = {
   Name: null,
   Email: null,
+  Kommentar: "",
   Sonnenblume: 0,
   Tulpe: 0,
   Lilie: 0,
@@ -63,12 +64,32 @@ document.getElementById("Bestellen").addEventListener("click", function () {
   } else {
     currentOrder.Email = document.getElementById("email").value;
     currentOrder.Name = document.getElementById("name").value;
+    currentOrder.Kommentar = document.getElementById("text").value;
     currentOrder.Gesammtsumme = `€ ${total}`;
     alert(
       "Ihre Produkte wurden bestellt. " +
         `siehe die Rechnung hier: ${document.getElementById("email").value}` +
         `\n ${JSON.stringify(currentOrder)}`
     );
+
+    //folgender Code wurde mit Hilfe des Internets (u.a. MDN) geschrieben. (die kommentare wurden von mir manuell dazugeschrieben)
+
+    // erstellung eines Blob objekts welches die momentane Bestellung als JSON string beinhaltet.
+    const blob = new Blob([JSON.stringify(currentOrder, null, 2)], {
+      type: "application/json",
+    });
+    //erstellung eines temporären Links welcher den Inhalt des Blobs hat.
+    const url = URL.createObjectURL(blob);
+    //erstellung eines <a> elements in dem HTML welches den Link beinhaltet
+    const a = document.createElement("a");
+    a.href = url;
+    //hier wird ein download mit dem element <a> verbunden welchen durch anclicken ausgelöst wird (dateinamen standardmäßig "[Name-des-Kunden]_Rechnung.json")
+    a.download = `Rechnung_${currentOrder.Name}.json`;
+    //simulierung eines User-cicks auf dem element <a> damit der download automatisch ausgelöst wird.
+    a.click();
+    //entfernung des temporären links
+    URL.revokeObjectURL(url);
+    //neuladen des Tabs um die Felder und den Waarenkorb zu leeren
     window.location.reload();
   }
 });
