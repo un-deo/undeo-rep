25be7617ea7c90fae47e30b3beea220f3d9842b5
undeo <dem230091@spengergasse.at>
2025-11-17 11:18:23 +0100

    Refactor Bruch class for improved error handling and add unit tests for various scenarios

--- 2025-09-03-3AHWII/2025-11-05-brueche/Program.cs
+++ 2025-09-03-3AHWII/2025-11-05-brueche/Program.cs
@@ -1,11 +1,19 @@
-﻿public class Program
+﻿using System;
+
+public class Program
 {
     public static void Main(string[] args)
     {
-        Bruch b1 = new Bruch(args[0]);
-        Bruch b2 = new Bruch(args[1]);
-        Bruch b3 = b1.addiere(b2);
-        Console.WriteLine("Ergebnis: " + b3.ToStringBruch());
+        try
+        {
+            Bruch b1 = new Bruch(args[0]);
+            Bruch b2 = new Bruch(args[1]);
+            Bruch b3 = b1.addiere(b2);
+            Console.WriteLine("Ergebnis: " + b3.ToStringBruch());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
-
 }
