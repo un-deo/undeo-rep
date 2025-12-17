54f72f315ecf9f8108ef1621607dbc7c7026cf7b
undeo <dem230091@spengergasse.at>
2025-12-17 07:45:34 +0100

    fibonacci üben

---
+++ 2025-09-03-3AHWII/2025-12-17-fibonacci-ubung/Programm/fib.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            int n = int.Parse(args[0]);
+            Console.WriteLine("Fibonacci-Zahl für n = " + n + " ist " + Fibonacci(n));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Bitte eine gültige ganze Zahl als Argument eingeben.");
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    public static int Fibonacci(int n)
+    {
+        if (n < 0)
+            throw new ArgumentOutOfRangeException("n darf nicht negativ sein.");
+        if (n == 1) return 0;
+        if (n == 2) return 1;
+
+        int a = 0, b = 1, c = 0;
+        for (int i = 3; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
