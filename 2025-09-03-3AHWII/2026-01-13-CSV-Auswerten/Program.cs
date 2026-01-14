f0eb44bd9a071659c8be7f3b86026414b4473e46
undeo <dem230091@spengergasse.at>
2026-01-14 10:57:12 +0100

    Enhance CSV reading by adding UTF-8 encoding and include unicode field in output

--- 2025-09-03-3AHWII/2026-01-13-CSV-Auswerten/Program.cs
+++ 2025-09-03-3AHWII/2026-01-13-CSV-Auswerten/Program.cs
@@ -1,23 +1,26 @@
 ﻿using CsvHelper;
 using CsvHelper.Configuration;
 using System.Globalization;
+using System.Text;
+using System.IO;
 
 class Program
 {
     static string[] persons = new string[100];
     static void Main()
     {
-        var filePath = "./persons.csv";
-        using (var reader = new StreamReader(filePath))
-        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";", HeaderValidated = null, MissingFieldFound = null }))
+        Console.OutputEncoding = Encoding.UTF8;
 
+        string filePath = "./persons.csv";
+        using (var reader = new StreamReader(filePath, Encoding.UTF8))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";", HeaderValidated = null, MissingFieldFound = null }))
         {
             var records = csv.GetRecords<Person>();
 
             int i = 0;
             foreach (var record in records)
             {
-                persons[i] = $"{i} - {record.Fullname} | {record.Email} | {record.Telefon} | {record.Adresse}";
+                persons[i] = $"{i} - {record.Fullname} | {record.Email} | {record.Telefon} | {record.Adresse} | {record.unicode}";
                 Console.WriteLine(persons[i]);
                 Console.WriteLine("--------------------------------------------------------------------------------");
                 i++;
@@ -31,13 +34,11 @@ class Program
 
 public class Person
 {
-    public string Fullname { get; set; }
-    public string Email { get; set; }
-    public string Telefon { get; set; }
-    public string Adresse { get; set; }
-    public string Unicode { get; set; }
+    public string? Fullname { get; set; }
+    public string? Email { get; set; }
+    public string? Telefon { get; set; }
+    public string? Adresse { get; set; }
+    public string? unicode { get; set; }
 }
 
-
-
-//hilfe bzw verständnis habe ich grossteils von hier: https://www.youtube.com/watch?v=z3BwMlcGdhg
+//hilfe bzw verständnis habe ich grossteils von hier: https://www.youtube.com/watch?v=z3BwMlcGdhgusing CsvHelper;
