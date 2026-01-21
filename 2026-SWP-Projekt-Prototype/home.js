776b571f228722b48713e4f0923eb0572a74fbc7
undeo <dem230091@spengergasse.at>
2026-01-21 11:15:56 +0100

    Add profile and seller dashboard pages with responsive design and Tailwind CSS

---
+++ 2026-SWP-Projekt-Prototype/home.js
@@ -0,0 +1,9 @@
+document.getElementById("anmeldenBtn").addEventListener("click", function () {
+  window.location.href = "anmelden.html";
+});
+
+document
+  .getElementById("registrierenBtn")
+  .addEventListener("click", function () {
+    window.location.href = "registrieren.html";
+  });
