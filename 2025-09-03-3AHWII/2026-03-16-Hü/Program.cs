67aa56f48cfd28bc914bd44a963b5cbce6415999
undeo <dem230091@spengergasse.at>
2026-03-16 19:12:28 +0100

    feat: add initial project structure and implement inventory system

---
+++ 2025-09-03-3AHWII/2026-03-16-Hü/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Inventar
+{
+    public interface IInventarGegenstand
+    {
+        string Name { get; }
+        string BeschreibeDich();
+    }
+
+    public class Waffe : IInventarGegenstand
+    {
+        public string Name { get; }
+        public int Schaden { get; }
+        public string Rarity { get; }
+
+        public Waffe(string name, int schaden, string rarity)
+        {
+            Name = name;
+            Schaden = schaden;
+            Rarity = rarity;
+        }
+
+        public string BeschreibeDich()
+        {
+            return $"Ich bin {Name} und mache {Schaden} Schaden. Seltenheit: {Rarity}";
+        }
+    }
+
+    public class Heiltrank : IInventarGegenstand
+    {
+        public string Name { get; }
+        public int Heilwert { get; }
+
+        public Heiltrank(string name, int heilwert)
+        {
+            Name = name;
+            Heilwert = heilwert;
+        }
+
+        public string BeschreibeDich()
+        {
+            return $"Ich bin {Name} und stelle {Heilwert} Lebenspunkte wieder her.";
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var inventar = new List<IInventarGegenstand>();
+
+            inventar.Add(new Waffe("Schwert", 15, "common"));
+            inventar.Add(new Heiltrank("Kleiner Heiltrank", 10));
+            inventar.Add(new Waffe("Excalibur", 500, "legendary"));
+            inventar.Add(new Heiltrank("Chug Splash", 20));
+
+            foreach (var gegenstand in inventar)
+            {
+                Console.WriteLine(gegenstand.BeschreibeDich());
+            }
+        }
+    }
+}
